afae815a357578a576f51237e55e30ca63bf3134
Eu sou <satuur.skid@gmail.com>
2021-04-29 22:49:45 -0300

    Update config.lua

---
+++ config.lua
@@ -0,0 +1,30 @@
+--[[
+   _____ .__                      _____.__       .__               __          
+  / ___ \|__| ____   ________ ___/ ____\__| ____ |__| ____   _____/  |_  ____  
+ / / ._\ \  |/    \ /  ___/  |  \   __\|  |/ ___\|  |/ __ \ /    \   __\/ __ \ 
+<  \_____/  |   |  \\___ \|  |  /|  |  |  \  \___|  \  ___/|   |  \  | \  ___/ 
+ \_____\ |__|___|  /____  >____/ |__|  |__|\___  >__|\___  >___|  /__|  \___  >
+                 \/     \/                     \/        \/     \/          \/ 
+[ PERMISSÕES PARA O ADMIN E A PM DEVEM SER ALTERADAS ABAIXO! ]
+[ SCRIPT PARA ENTRAR OU SAIR DE PATRULHA ]
+- Os scripts podem ser encontrados na nossa comunidade pelo Discord ou GitHub. Links abaixo.
+- Não aceite scripts caros, você pode nos contatar para fazer um plugin por um preço acessível.
+Links para a nossa comunidade! 🙂
+{
+	"GitHub": "https://github.com/insuficiente",
+	"Discord": "https://discord.gg/jqpmr5FRuT"	
+}
+Atualização: 29/04/2021 | v1.21.1
+Não tente revender este script/plugin, é totalmente feito pela nossa comunidade e não por você.
+Seria bem feio da sua parte vender um plugin gratuito, hein?
+DESEJAMOS UM ÓTIMO ROLEPLAY PARA A SUA CIDADE. ESPERAMOS QUE OS SEUS PLAYERS TENHAM A MELHOR EXPERIÊNCIA COM OS NOSSOS SCRIPTS.
+]]
+local Config = {}
+
+-- Permissão do PM
+Config.Pm_Perm = "pm01.permissao"
+
+-- Permissão do Admin
+Config.Adm_Perm = "admin.permissao"
+
+return Config
